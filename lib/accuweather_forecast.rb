d9cecc1d49f583057302d6dae9dd721b75a21cf1
Andres Matte <matte.andres@gmail.com>
2016-11-21 20:21:28 -0300

    Added 10 minutes cache

--- lib/accuweather_forecast.rb
+++ lib/accuweather_forecast.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 
+# Accuweather Forecast API wrapper
 module AccuweatherForecast
+  # When no city is provided, these cities forecasts are shown.
   DEFAULT_CITIES = ['Paris', 'Bangkok', 'Santiago', 'New York', 'Bamako'].freeze
+  # Weather doesn't change so frequently, so let's cache it :)
+  CACHE_EXPIRE_TIME = 60 * 10 # 10 minutes
 
   FORECAST_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/'\
                   '%{location_key}?apikey=%{api_key}&metric=true'.freeze
@@ -28,15 +32,25 @@ module AccuweatherForecast
   private
 
   def request_forecast(city)
-    # Get city location key
-    url = SEARCH_URL % { city: city,
-                         api_key: ENV['ACID_FORECAST_ACCUWEATHER_KEY'] }
-    location_key = HTTParty.get(url).parsed_response[0]['Key']
+    search_url = SEARCH_URL % { city: city,
+                                api_key: ENV['ACID_FORECAST_ACCUWEATHER_KEY'] }
+    response = Redis.current.get(search_url)
+    if response.nil?
+      # Get city location key
+      location_key = HTTParty.get(search_url).parsed_response[0]['Key']
 
-    # Get 5 days forecast
-    url = FORECAST_URL % { location_key: location_key,
-                           api_key: ENV['ACID_FORECAST_ACCUWEATHER_KEY'] }
+      # Get 5 days forecast
+      url = FORECAST_URL % { location_key: location_key,
+                             api_key: ENV['ACID_FORECAST_ACCUWEATHER_KEY'] }
+      response = HTTParty.get(url).parsed_response.to_json
+      
+      cache_response(search_url, response)
+    end
+    JSON.parse(response)
+  end
 
-    HTTParty.get(url).parsed_response
+  def cache_response(key, response)
+    Redis.current.set(key, response)
+    Redis.current.expire(key, CACHE_EXPIRE_TIME)
   end
 end
