26f4b50e815de254ce7b1830f19d5ffd3931ce3b
Andres Matte <matte.andres@gmail.com>
2016-11-24 10:09:47 -0300

    Add environment variables validation

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
+raise ArgumentError.new("Redis URL not provided. Set up ENV['REDIS_URL'].") unless ENV['REDIS_URL']
 Redis.current = Redis.new(url: ENV['REDIS_URL'])
--- lib/accuweather_forecast.rb
+++ lib/accuweather_forecast.rb
@@ -16,6 +16,7 @@ class AccuweatherForecast
 
   def initialize(api_key)
     @api_key = api_key
+    validate!
   end
 
   # Return 5 days forecast for a city.
@@ -36,14 +37,18 @@ class AccuweatherForecast
 
   private
 
+  def validate!
+    missing_api_key = "Api key not provided. Set up ENV['ACID_FORECAST_ACCUWEATHER_KEY']"
+    raise ArgumentError.new(missing_api_key) unless @api_key
+  end
+
   def request_forecast(city)
     response = Redis.current.get(city)
     if response.nil?
       location_key = location_key(city)
       unless location_key.nil?
         # Get 5 days forecast
-        url = FORECAST_URL % { location_key: location_key,
-                               api_key: @api_key }
+        url = FORECAST_URL % { location_key: location_key, api_key: @api_key }
         response = HTTParty.get(url).parsed_response.to_json
         cache_response(city, response)
       end
@@ -59,8 +64,7 @@ class AccuweatherForecast
 
   # Get city location key
   def location_key(city)
-    search_url = SEARCH_URL % { city: city,
-                                api_key: @api_key }
+    search_url = SEARCH_URL % { city: city, api_key: @api_key }
     response = HTTParty.get(search_url).parsed_response
     !response.empty? ? response[0]['Key'] : nil
   end
