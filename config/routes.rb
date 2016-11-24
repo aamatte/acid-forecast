d60415445cb3aa97e5223642e752d570da916e1b
Andres Matte <matte.andres@gmail.com>
2016-11-24 10:41:01 -0300

    Custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with 'rake routes'.
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with 'root'
   root 'forecast#index'
   get 'forecast', to: 'forecast#index'
-  get '*path', to: 'forecast#index' # TODO: redirect to 404 page
+
+  %w(404 500).each do |code|
+    get code, to: 'errors#show', code: code
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
